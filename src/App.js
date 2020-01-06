9595806b61467f22456ce304a969213f23c3ad16
fernanda <fernanda.marroquin07@gmail.com>
2020-01-06 17:50:46 -0500

    adds snake and movement

--- src/App.js
+++ src/App.js
@@ -11,21 +11,92 @@ const randomPosition = () => {
   return [x,y]
 }
 
+const initialState = {
+  food: randomPosition(),
+  speed: 500,
+  direction: 'right',
+  snake: [
+    [0,0],
+    [2,0]
+  ]
+}
+
 class App extends Component {
-  state = {
-    food: randomPosition(),
-    snake: [
-      [0,0],
-      [2,0]
-    ]
+  state = initialState
+
+  componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed)
+    document.onkeydown = this.onKeyDown;
   }
+
+  componentDidUpdate() {
+    this.checkBorders();
+  }
+
+  onKeyDown = (e) => {
+    e = e || window.event;
+    switch(e.keyCode) {
+      case 38:
+        this.setState({direction: 'up'});
+      break;
+      case 40:
+        this.setState({direction: 'down'});
+      break;
+      case 37:
+        this.setState({direction: 'left'});
+      break;
+      case 39:
+        this.setState({direction: 'right'});
+      break;
+      default:
+    }
+  }
+
+  moveSnake = () => {
+    let dots = [...this.state.snake];
+    let head = dots[dots.length - 1];
+    switch(this.state.direction) {
+      case 'right':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'left':
+        head = [head[0] - 2, head[1]];
+        break;
+      case 'down':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'up':
+        head = [head[0], head[1] - 2];
+        break;
+      default:
+    }
+    // add head
+    dots.push(head);
+    // remove tail, first item in arr
+    dots.shift()
+    this.setState({
+      snake: dots
+    })
+  }
+
+  checkBorders() {
+    let head = this.state.snake[this.state.snake.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      this.gameOver();
+    }
+  }
+
+  gameOver() {
+    alert('GAME OVER');
+    this.setState(initialState)
+  }
+
   render() {
     return (
       <div className='game-area'>
         <Snake snake={this.state.snake} />
         <Food dot={this.state.food} />
       </div>
-      
     );
   }
 }
