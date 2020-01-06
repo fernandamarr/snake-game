060169de8147bc1a14bd3645b43d956ff6abe742
fernanda <fernanda.marroquin07@gmail.com>
2020-01-06 18:46:05 -0500

    adds build

---
+++ build/precache-manifest.b590524a120920778b2f6dbe1539d566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9554555c3d620a4b059d4aed03c4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1889ec750e05e44e7c38",
+    "url": "/static/css/main.a40ed83e.chunk.css"
+  },
+  {
+    "revision": "7cd2654833be31bb718b",
+    "url": "/static/js/2.9ea0fe9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9ea0fe9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1889ec750e05e44e7c38",
+    "url": "/static/js/main.839f8d22.chunk.js"
+  },
+  {
+    "revision": "e51a4f8818155d2d4658",
+    "url": "/static/js/runtime-main.dc69c5b8.js"
+  }
+]);
